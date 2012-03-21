f19efc54e0c321f88a4b05715608b7ac21b9e0f4
Morten Kjaergaard <mkjargaard@willowgarage.com>
2012-03-21 13:39:24 -0700

    update to simpler component constructor

--- procedure1/src/client_component.cpp
+++ procedure1/src/client_component.cpp
@@ -1,14 +1,10 @@
-#include <darc/component.h>
-#include <darc/procedure/client.h>
-#include <darc/timer/periodic_timer.h>
-
+#include <darc/darc.h>
 #include <std_msgs/Int32.h>
 
 class MyClientComponent : public darc::Component
 {
 protected:
   darc::procedure::Client<std_msgs::Int32, std_msgs::Int32, std_msgs::Int32> procedure_client_;
-  //  darc::procedure::EventListener listener_;
   darc::timer::PeriodicTimer timer_;
 
 protected:
@@ -28,23 +24,15 @@ protected:
   {
     DARC_INFO("Result %s", call_id.short_string().c_str());
   }
-  /*
-  void remoteProcedureChanges(const darc::procedure::AdvertisedProcedureInfo& info, const size_t& num)
-  {
-    DARC_AUTOTRACE();
-  }
-  */
+
 public:
-  MyClientComponent( const std::string& instance_name, darc::Node::Ptr node ) :
-    darc::Component(instance_name, node),
-    procedure_client_( this,
-		       "myprocedure",
-		       boost::bind( &MyClientComponent::returnHandler, this, _1, _2 ),
-		       boost::bind( &MyClientComponent::statusHandler, this, _1, _2 ) ),
-    //    listener_(this),
-    timer_( this, boost::bind(&MyClientComponent::timerHandler, this), boost::posix_time::seconds(1) )
+  MyClientComponent() :
+    procedure_client_(this,
+		      "myprocedure",
+		      boost::bind( &MyClientComponent::returnHandler, this, _1, _2 ),
+		      boost::bind( &MyClientComponent::statusHandler, this, _1, _2 ) ),
+    timer_(this, &MyClientComponent::timerHandler, boost::posix_time::seconds(1))
   {
-    //listener_.remoteProcedureChangesListen(boost::bind(&MyClientComponent::remoteProcedureChanges, this, _1, _2));
   }
 
 };
--- procedure1/src/server_component.cpp
+++ procedure1/src/server_component.cpp
@@ -1,7 +1,4 @@
-#include <darc/node.h>
-#include <darc/component.h>
-#include <darc/procedure/server.h>
-
+#include <darc/darc.h>
 #include <std_msgs/Int32.h>
 
 class MyServerComponent : public darc::Component
@@ -18,8 +15,7 @@ protected:
   }
 
 public:
-  MyServerComponent( const std::string& instance_name, darc::Node::Ptr node ) :
-    darc::Component(instance_name, node),
+  MyServerComponent() :
     procedure_server_( this, "myprocedure", boost::bind(&MyServerComponent::procedureCall, this, _1, _2) )
   {
     DARC_AUTOTRACE();
