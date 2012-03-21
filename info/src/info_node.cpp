c827a9ff0c5536b36f7730435ce7bf2e58169258
Morten Kjaergaard <mkjargaard@willowgarage.com>
2012-03-21 14:12:28 -0700

    added info component

---
+++ info/src/info_node.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <darc/darc.h>
+
+int main(int argc, const char* argv[])
+{
+  // Create Node
+  darc::Node::Ptr node = darc::Node::create();
+
+  // Set up node connections manually
+  node->accept("udp://127.0.0.1:5120-5125");
+  node->connect("udp://127.0.0.1:5120-5125");
+
+  // Instantiate Components
+  darc::Component::Ptr c1 = node->instantiateComponent("MyInfoComponent");
+
+  // Run Components (These are non-blocking)
+  c1->run();
+
+  // Run Node in main thread
+  node->run(); // This is blocking
+  return 0;
+}
---
+++ info/src/subscriber_component.cpp
@@ -0,0 +1,27 @@
+#include <darc/component.h>
+#include <darc/pubsub/subscriber.h>
+#include <darc/log.h>
+
+#include <std_msgs/String.h>
+
+class MySubscriberComponent : public darc::Component
+{
+protected:
+  darc::pubsub::Subscriber<std_msgs::String> sub_;
+
+protected:
+  void subHandler( const boost::shared_ptr<const std_msgs::String> msg )
+  {
+    DARC_INFO("Received Message: %s", msg->data.c_str());
+  }
+
+public:
+  MySubscriberComponent( const std::string& instance_name, darc::Node::Ptr node ) :
+    darc::Component(instance_name, node),
+    sub_(this, "/mytopic", boost::bind(&MySubscriberComponent::subHandler, this, _1) )
+  {
+  }
+
+};
+
+DARC_REGISTER_COMPONENT(MySubscriberComponent)
