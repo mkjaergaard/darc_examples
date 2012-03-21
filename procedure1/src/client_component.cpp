d75130d8587ec13a7a5f0200e159b7e05ddb1e25
Morten Kjaergaard <mkjargaard@willowgarage.com>
2012-03-21 13:29:49 -0700

    updated procedure1 example to new darc version

--- procedure1/src/client_component.cpp
+++ procedure1/src/client_component.cpp
@@ -8,6 +8,7 @@ class MyClientComponent : public darc::Component
 {
 protected:
   darc::procedure::Client<std_msgs::Int32, std_msgs::Int32, std_msgs::Int32> procedure_client_;
+  //  darc::procedure::EventListener listener_;
   darc::timer::PeriodicTimer timer_;
 
 protected:
@@ -18,16 +19,21 @@ protected:
     procedure_client_.call(msg);
   }
 
-  void statusHandler(const darc::procedure::CallID& call_id, boost::shared_ptr<std_msgs::Int32> msg)
+  void statusHandler(const darc::procedure::CallID& call_id, boost::shared_ptr<const std_msgs::Int32> msg)
   {
-    DARC_INFO("Feedback Call %s", call_id.short_string().c_str());
+    DARC_INFO("Feedback %s", call_id.short_string().c_str());
   }
 
-  void returnHandler(const darc::procedure::CallID& call_id, boost::shared_ptr<std_msgs::Int32> msg)
+  void returnHandler(const darc::procedure::CallID& call_id, boost::shared_ptr<const std_msgs::Int32> msg)
   {
-    DARC_INFO("Result Call %s", call_id.short_string().c_str());
+    DARC_INFO("Result %s", call_id.short_string().c_str());
   }
-
+  /*
+  void remoteProcedureChanges(const darc::procedure::AdvertisedProcedureInfo& info, const size_t& num)
+  {
+    DARC_AUTOTRACE();
+  }
+  */
 public:
   MyClientComponent( const std::string& instance_name, darc::Node::Ptr node ) :
     darc::Component(instance_name, node),
@@ -35,8 +41,10 @@ public:
 		       "myprocedure",
 		       boost::bind( &MyClientComponent::returnHandler, this, _1, _2 ),
 		       boost::bind( &MyClientComponent::statusHandler, this, _1, _2 ) ),
+    //    listener_(this),
     timer_( this, boost::bind(&MyClientComponent::timerHandler, this), boost::posix_time::seconds(1) )
   {
+    //listener_.remoteProcedureChangesListen(boost::bind(&MyClientComponent::remoteProcedureChanges, this, _1, _2));
   }
 
 };
--- procedure1/src/server_component.cpp
+++ procedure1/src/server_component.cpp
@@ -10,7 +10,7 @@ protected:
   darc::procedure::Server<std_msgs::Int32, std_msgs::Int32, std_msgs::Int32> procedure_server_;
 
 protected:
-  void procedureCall(const darc::procedure::CallID& call_id, boost::shared_ptr<std_msgs::Int32> msg)
+  void procedureCall(const darc::procedure::CallID& call_id, boost::shared_ptr<const std_msgs::Int32> msg)
   {
     DARC_INFO("Procedure Call %s", call_id.short_string().c_str());
     procedure_server_.feedback(call_id, msg);
@@ -22,6 +22,7 @@ public:
     darc::Component(instance_name, node),
     procedure_server_( this, "myprocedure", boost::bind(&MyServerComponent::procedureCall, this, _1, _2) )
   {
+    DARC_AUTOTRACE();
   }
 
 };
