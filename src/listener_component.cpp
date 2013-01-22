31ef08853269d526e73afa5be5f96d9ba6431bc5
Morten Kjaergaard <woobiz@gmail.com>
2013-01-22 18:37:30 +0100

    changed example to chatter/listener

---
+++ src/listener_component.cpp
@@ -0,0 +1,23 @@
+#include <iris/static_scope.hpp>
+#include <darc/component.hpp>
+#include <darc/subscriber.h>
+
+class listener_component : public darc::component, public iris::static_scope<iris::Info>
+{
+  darc::subscriber<std::string> sub_;
+
+  void chatter_callback(const boost::shared_ptr<const std::string> msg)
+  {
+    slog<iris::Info>("Received",
+		     "msg", iris::arg<std::string>(*msg));
+  }
+
+public:
+  listener_component() :
+    sub_(this, "chatter", boost::bind(&listener_component::chatter_callback, this, _1))
+  {
+  }
+
+};
+
+DARC_REGISTER_COMPONENT(listener_component)
---
+++ src/talker_component.cpp
@@ -0,0 +1,34 @@
+#include <iris/static_scope.hpp>
+#include <darc/component.hpp>
+#include <darc/periodic_timer.hpp>
+#include <darc/publisher.h>
+
+class talker_component : public darc::component, public iris::static_scope<iris::Info>
+{
+  int count_;
+  darc::periodic_timer timer_;
+  darc::publisher<std::string> chatter_pub_;
+
+  void timer_callback()
+  {
+    boost::shared_ptr<std::string> msg = boost::make_shared<std::string>();
+
+    *msg = std::string("Hello World ").append(boost::to_string(++count_));
+
+    slog<iris::Info>("Publishing",
+		     "Msg", iris::arg<std::string>(*msg));
+
+    chatter_pub_.publish(msg);
+  }
+
+public:
+  talker_component() :
+    count_(0),
+    timer_(this, &talker_component::timer_callback, boost::posix_time::seconds(1)),
+    chatter_pub_(this, "chatter")
+  {
+  }
+
+};
+
+DARC_REGISTER_COMPONENT(talker_component)
